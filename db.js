08bf27d19159e04a4b235c9ecd5832021dee3136
qosburn <qosburn@mac.com>
2021-11-15 12:29:20 -0500

    removed dialect

--- db.js
+++ db.js
@@ -3,15 +3,16 @@ const { Sequelize } = require('sequelize');
 const Sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   // Added
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false,
+  //   },
     // End Add
   },
 });
 
+// old stuff
 // const db = new Sequelize(process.env.DATABASE_URL, {
 //   dialect: 'postgres',
 //   // Added
